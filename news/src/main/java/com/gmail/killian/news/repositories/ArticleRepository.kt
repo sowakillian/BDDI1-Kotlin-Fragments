75dab60a305b3936c9766c7777c7556da3eef802
Sowa Killian <killian.sowa@m6.fr>
2020-04-22 08:25:42 +0200

    HttpLoger added

--- news/src/main/java/com/gmail/killian/news/repositories/ArticleRepository.kt
+++ news/src/main/java/com/gmail/killian/news/repositories/ArticleRepository.kt
@@ -1,16 +1,33 @@
 package com.gmail.killian.news.repositories
 
+import com.gmail.killian.news.BuildConfig
 import com.gmail.killian.news.model.Article
 import com.gmail.killian.news.services.ArticleService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ArticleRepository {
     private val service: ArticleService
     init {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            })
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         val retrofit = Retrofit.Builder().apply {
             baseUrl("https://newsapi.org")
         }.addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
         service = retrofit.create(ArticleService::class.java)
     }
